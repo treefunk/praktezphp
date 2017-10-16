882d564a5638adda13b34d7a292769663242d00f
Jhondee <jhondeediaz@gmail.com>
2017-10-16 11:47:27 +0800

    added contact form

---
+++ basiccontactform/index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact me</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <form action="" method="post">
+        <div class="form-group"><label for="title">Title:</label><input type="text" name="title" class="form-control"></div>
+        <div class="form-group"><label for="email">Email:</label><input type="text" name="email" class="form-control"></div>
+        <div class="form-group"><label for="body">Body:</label><input type="text" name="body" class="form-control"></div>
+        <button class="btn" type="submit">Submit</button>
+    </form>
+</div>
+</body>
+</html>
